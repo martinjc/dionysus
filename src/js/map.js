4405da06a2736cc8b46b98851ecdd501e51badde
Martin Chorley <martin.chorley@gmail.com>
2013-04-20 11:09:49 +0100

    map working with openstreetmap tiles

--- src/js/map.js
+++ src/js/map.js
@@ -1,7 +1,7 @@
 /* global L */
 var map = L.map('map');
 
-var tileUrl='http://{s}.tile.cloudmade.com/364b324514d240c8afdd7ba132fd4841/997/256/{z}/{x}/{y}.png';
+var tileUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 var attrib='Map data © OpenStreetMap contributors';
 var tileLayer = new L.TileLayer(tileUrl, {minZoom: 8, maxZoom: 18, attribution: attrib});
 
