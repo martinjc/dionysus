39aa359361c80952ec6400ab170eb6b041a46ff5
Martin Chorley <martin.chorley@gmail.com>
2013-04-20 13:18:58 +0100

    having to move past untappd api limits

--- src/js/map.js
+++ src/js/map.js
@@ -1,6 +1,29 @@
-/* global L, console, client_id, client_secret */
+/* global L, console, foursq_client_id, foursq_client_secret, untappd_client_id, untappd_client_secret */
 (function(){
 var map = L.map('map');
+var pubs = [];
+
+function add_pub(venue_data) {
+    var marker = new L.marker([venue_data.location.lat, venue_data.location.lng], {
+        title: venue_data.name
+    }).addTo(map);
+    pubs.append(marker);
+    console.log(venue_data);
+
+    /* Untappd stupid API limit
+    var fsq_id = venue_data.id;
+    $.getJSON("http://api.untappd.com/v4/venue/foursquare_lookup/" + fsq_id, {
+        client_id: untappd_client_id,
+        client_secret: untappd_client_secret
+    }).done(function(data){
+        console.log(data);
+        console.log(data.response.venue.items[0].venue_id);
+    }).fail(function(data) {
+        console.log('oh no');
+    });
+    */
+}
+
 
 function showError(ev) {
     console.log('not implemented yet! - showError ' + ev);
@@ -20,16 +43,18 @@ function onMoveEnd() {
     var ll = center.lat + "," + center.lng;
     $.getJSON("https://api.foursquare.com/v2/venues/search", {
         ll: ll,
-        radius: 1000,
+        radius: 500,
         limit: 50,
-        intent: "browse",
+        intent: "checkin",
         categoryId: "4bf58dd8d48988d155941735,4bf58dd8d48988d11b941735,4d4b7105d754a06376d81259",
-        client_id: client_id,
-        client_secret: client_secret,
+        client_id: foursq_client_id,
+        client_secret: foursq_client_secret,
         v: 20130420
     })
     .done(function(data) {
-        console.log(data);
+        $.each(data.response.venues, function(i, item) {
+            add_pub(item);
+        });
     });
 }
 
