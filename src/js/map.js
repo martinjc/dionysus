bfbd1faf8e9fdd82830d084e97146112555e9f5c
Martin Chorley <martin.chorley@gmail.com>
2013-04-20 11:56:14 +0100

    got some foursquare retrieval shit going on

--- src/js/map.js
+++ src/js/map.js
@@ -1,10 +1,51 @@
-/* global L */
+/* global L, console, client_id, client_secret */
+(function(){
 var map = L.map('map');
 
-var tileUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-var attrib='Map data © OpenStreetMap contributors';
-var tileLayer = new L.TileLayer(tileUrl, {minZoom: 8, maxZoom: 18, attribution: attrib});
+function showError(ev) {
+    console.log('not implemented yet! - showError ' + ev);
+}
 
-map.addLayer(tileLayer);
+function onLocationError(ev) {
+    showError(ev);
+}
 
-map.locate({setView: true, maxZoom: 18});
+function onLocationFound() {
+    console.log('not implemented yet! - onLocationFound');
+}
+
+function onMoveEnd() {
+    var center = map.getCenter();
+    console.log(center);
+    var ll = center.lat + "," + center.lng;
+    $.getJSON("https://api.foursquare.com/v2/venues/search", {
+        ll: ll,
+        radius: 1000,
+        limit: 50,
+        intent: "browse",
+        categoryId: "4bf58dd8d48988d155941735,4bf58dd8d48988d11b941735,4d4b7105d754a06376d81259",
+        client_id: client_id,
+        client_secret: client_secret,
+        v: 20130420
+    })
+    .done(function(data) {
+        console.log(data);
+    });
+}
+
+
+function initmap() {
+    var tileUrl='http://{s}.tile.cloudmade.com/364b324514d240c8afdd7ba132fd4841/997/256/{z}/{x}/{y}.png';
+    var attrib='Map data © OpenStreetMap contributors';
+    var tileLayer = new L.TileLayer(tileUrl, {minZoom: 8, maxZoom: 18, attribution: attrib});
+
+    map.addLayer(tileLayer);
+    map.locate({setView: true, maxZoom: 17});
+
+    map.on('locationerror', onLocationError);
+    map.on('locationfound', onLocationFound);
+    map.on('moveend', onMoveEnd);
+}
+
+initmap();
+})();
